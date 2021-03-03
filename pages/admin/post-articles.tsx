42ea4026c5ed1d99541912571760035f40153763
syoichi kimita <kimi.syou.koma@gmail.com>
2021-03-03 09:05:04 +0900

    #1 - on the way create post function

---
+++ pages/admin/post-articles.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+
+const PostArticles: React.FC = () => {
+  const {register, handleSubmit, watch, errors} = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name="article" defaultValue="input your text" ref={register} />
+        <input name="another" defaultValue="input your another" ref={register({required: true})} />
+        {errors.another && <span>This filed is required.</span>}
+        <input type="submit" />
+      </form>
+    </>
+  )
+};
+
+export default PostArticles;
